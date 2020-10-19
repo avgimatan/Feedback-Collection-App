8dfec74b9111d054d95937317650ecdb8aa5587b
Mavgi <avgimatan@gmail.com>
2020-10-19 10:33:21 +0300

    finish mailer conf

--- config/dev.js
+++ config/dev.js
@@ -6,5 +6,6 @@ module.exports = {
     cookieKey : 'peifbghgtidhgmjldoswgrvgmgudgndkfg',
     stripePublishableKey: 'pk_test_51HVJkiIIRRW9BF1QAZ66EeXmk5v4t7A3aUsl20mNtD1AIoCTzGCL64x3XIOq4AaR1rn4NnJR5saOgaj8dhIXMPU700Hc935iUS',
     stripeSecretKey: 'sk_test_51HVJkiIIRRW9BF1QW5mJtFOcm1yQCuTDnwrCcbmgSXZZuSLOK0eR4zkqqljPzOpfTklSz1IyBx8XF74FEzW1GiCJ00JnZ8CXqv',
-    sendGridKey: "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"
+    sendGridKey: "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",
+    redirectDomain: "http://localhost:3000"
 };
