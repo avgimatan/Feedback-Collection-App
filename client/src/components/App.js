5005d481432f485cf051be745eba6eeb87746ac0
Mavgi <avgimatan@gmail.com>
2020-11-29 12:02:59 +0200

    Survey component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,8 @@ import * as actions from "../actions";
 
 import Header from "./Header";
 import Landing from "./Landing";
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import Dashboard from "./Dashboard";
+import SurveyNew from "./surveys/SurveyNew";
 
 // Route is only for define different urls in the app
 class App extends Component {
